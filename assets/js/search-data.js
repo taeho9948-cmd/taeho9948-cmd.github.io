93851ccd89010a11877248a2c8b87f2c1a567473
taeho9948-cmd <taeho9948@gmail.com>
2025-12-18 06:29:12 +0000

    Deploying to gh-pages from @ nbml-dgu/nbml-dgu.github.io@04bb590ffcfc052f306eab719ad5471b72f54c5a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "CV",
           description: "Curriculum Vitae of Prof. Jae-Min Oh",
           section: "Navigation",
